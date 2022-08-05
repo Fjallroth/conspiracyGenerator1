634c243a86695f1d53a5a1d164868e26e8b77169
Fjallroth <christer.fjallroth@gmail.com>
2022-08-05 16:00:35 +0200

    changed server.js

--- clientSideCode/js/main.js
+++ clientSideCode/js/main.js
@@ -2,7 +2,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
     try{
-        const response = await fetch(`https://git.heroku.com/conspiracy-generator-100devs.git/api`)
+        const response = await fetch(`https://conspiracy-generator-100devs.herokuapp.com/api`)
         const data = await response.json()
 
         console.log(data)
