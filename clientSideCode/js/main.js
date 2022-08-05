642acc163ca08945475544282ff0098ffe757138
Fjallroth <christer.fjallroth@gmail.com>
2022-08-05 15:45:16 +0200

    changed URL

--- clientSideCode/js/main.js
+++ clientSideCode/js/main.js
@@ -2,7 +2,7 @@ document.querySelector('button').addEventListener('click', apiRequest)
 
 async function apiRequest(){
     try{
-        const response = await fetch(`http://localhost:8000/api`)
+        const response = await fetch(`https://git.heroku.com/conspiracy-generator-100devs.git/api`)
         const data = await response.json()
 
         console.log(data)
